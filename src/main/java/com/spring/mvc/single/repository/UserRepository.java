6df577effd4a5c9e629869542f896e7f00b75d75
stanley1208 <stanley.mei08@gmail.com>
2021-10-04 22:04:37 +0800

    java

--- src/main/java/com/spring/mvc/single/repository/UserRepository.java
+++ src/main/java/com/spring/mvc/single/repository/UserRepository.java
@@ -11,8 +11,8 @@ import com.spring.mvc.single.entity.User;
 
 /*
  * 方法命名規則
- * 1.查詢方法以find | read | get 開頭
- * 2.
+ * 1. 查詢方法以 find | read | get 開頭
+ * 2. 涉及條件查詢時，條件的屬性（首字母大寫）用條件關鍵字（例如：And、Or）連結
  * */
 
 @Repository
