edaaf5c0402ec79e96445a115eeb7fb68044d214
stanley1208 <stanley.mei08@gmail.com>
2021-10-04 20:37:02 +0800

    java

--- src/main/java/com/spring/mvc/single/controller/UserController.java
+++ src/main/java/com/spring/mvc/single/controller/UserController.java
@@ -9,9 +9,12 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,85 +27,106 @@ import com.spring.mvc.single.repository.UserRepository;
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
-	
-	//新增範例資料
+
+	// 新增範例資料
 	@GetMapping("/test/create_sample_data")
 	@ResponseBody
 	public String testCreareSampleData() {
-		Random r=new Random();
-		Faker faker=new Faker();
-		int count=150;
-		for(int i=0;i<count;i++) {
-			User user=new User();
+		Random r = new Random();
+		Faker faker = new Faker();
+		int count = 150;
+		for (int i = 0; i < count; i++) {
+			User user = new User();
 			user.setName(faker.name().lastName());
-			user.setPassword(String.format("%04d",r.nextInt(10000)));
+			user.setPassword(String.format("%04d", r.nextInt(10000)));
 			user.setBirth(faker.date().birthday());
-			//儲存到資料庫
+			// 儲存到資料庫
 			userRepository.saveAndFlush(user);
 		}
-		
+
 		return "Create sample data ok !";
 	}
-	
-	//查詢範例資料 1
+
+	// 查詢範例資料 1
 	@GetMapping("/test/findall")
 	@ResponseBody
 	public List<User> testFindall() {
-		List<User>users=userRepository.findAll();
-		
+		List<User> users = userRepository.findAll();
+
 		return users;
 	}
-	
-	
-	//查詢範例資料 2
+
+	// 查詢範例資料 2
 	@GetMapping("/test/findall_sort")
 	@ResponseBody
 	public List<User> testFindallSort() {
-		//ASC自然排序 小到大,DESC 大到小
-		Sort sort=new Sort(Sort.Direction.ASC,"name");
-		List<User>users=userRepository.findAll();
-			
-			return users;
-		}
-	
-	//查詢範例資料 3
-		@GetMapping("/test/findall_ids")
-		@ResponseBody
-		public List<User> testFindallIds() {
-			//查找指定id的資料
-			Iterable<Long>ids=Arrays.asList(1L,3L,5L);
-			List<User>users=userRepository.findAll(ids);
-				
-				return users;
-			}
-		
-		//查詢範例資料 4
-		@GetMapping("/test/findall_example")
-		@ResponseBody
-		public List<User> testFindallExample() {
-			User user=new User();
-			user.setId(2L);
-			user.setPassword("1234");
-			//根據Example 所提供的實例(例如:user)來查詢
-			Example<User>example=Example.of(user);
-			List<User>users=userRepository.findAll(example);
-						
-				return users;
-			}
-		
-		//查詢範例資料 4
-		@GetMapping("/test/findall_example2")
-		@ResponseBody
-		public List<User> testFindallExample2() {
-			User user=new User();
-			user.setName("a");
-			//name的內容是否有包含"a"
-			//建立ExampleMatcher比對器
-			ExampleMatcher matcher=ExampleMatcher.matching()
-					.withMatcher("name", ExampleMatcher.GenericPropertyMatchers.contains());
-			Example<User>example=Example.of(user,matcher);
-			List<User>users=userRepository.findAll(example);
-								
-			return users;
-			}
+		// ASC自然排序 小到大,DESC 大到小
+		Sort sort = new Sort(Sort.Direction.ASC, "name");
+		List<User> users = userRepository.findAll();
+
+		return users;
+	}
+
+	// 查詢範例資料 3
+	@GetMapping("/test/findall_ids")
+	@ResponseBody
+	public List<User> testFindallIds() {
+		// 查找指定id的資料
+		Iterable<Long> ids = Arrays.asList(1L, 3L, 5L);
+		List<User> users = userRepository.findAll(ids);
+
+		return users;
+	}
+
+	// 查詢範例資料 4
+	@GetMapping("/test/findall_example")
+	@ResponseBody
+	public List<User> testFindallExample() {
+		User user = new User();
+		user.setId(2L);
+		user.setPassword("1234");
+		// 根據Example 所提供的實例(例如:user)來查詢
+		Example<User> example = Example.of(user);
+		List<User> users = userRepository.findAll(example);
+
+		return users;
+	}
+
+	// 查詢範例資料 4
+	@GetMapping("/test/findall_example2")
+	@ResponseBody
+	public List<User> testFindallExample2() {
+		User user = new User();
+		user.setName("a");
+		// name的內容是否有包含"a"
+		// 建立ExampleMatcher比對器
+		ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("name",
+				ExampleMatcher.GenericPropertyMatchers.contains());
+		Example<User> example = Example.of(user, matcher);
+		List<User> users = userRepository.findAll(example);
+
+		return users;
+	}
+
+	// 查詢範例資料 4
+	@GetMapping("/test/get_one")
+	@ResponseBody
+	public User getOne() {
+
+		return userRepository.findOne(3L);
+	}
+
+	// 查詢分頁
+	@GetMapping("/test/page/{no}")
+	@ResponseBody
+	public List<User> testPage(@PathVariable("no") Integer no) {
+		int pageNo = no;
+		int pageSize = 10;
+		// 分頁請求
+		PageRequest pageRequest = new PageRequest(pageNo, pageSize);
+		Page<User> page = userRepository.findAll(pageRequest);
+
+		return page.getContent();
+	}
+
 }
