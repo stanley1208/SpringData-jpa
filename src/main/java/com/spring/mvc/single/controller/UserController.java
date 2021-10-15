0dc8383da3d135a0ec1c626aa450df2e4926a04a
stanley1208 <stanley.mei08@gmail.com>
2021-10-15 20:49:42 +0800

    java

--- src/main/java/com/spring/mvc/single/controller/UserController.java
+++ src/main/java/com/spring/mvc/single/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.spring.mvc.single.controller;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 import java.util.Arrays;
 import java.util.Date;
@@ -92,6 +93,28 @@ public class UserController {
 			model.addAttribute("_method", "DELETE");
 			return "user/index"; // 重導到 /WEB-INF/view/user/index.jsp
 		}
+		
+		// 查詢分頁
+		// 路徑範例:/page,/page?no=1, /page?no=10 etc...
+		@GetMapping("/page")
+		public String userPage(Model model,@RequestParam(name = "no",required = false,defaultValue = "0") Integer no) {
+			int pageNo = no;
+			int pageSize = 10;
+			// 排序
+			Sort.Order order = new Sort.Order(Sort.Direction.ASC, "name");
+			
+			Sort sort = new Sort(order);
+
+			// 分頁請求
+			PageRequest pageRequest = new PageRequest(pageNo, pageSize, sort);
+			Page<User> page = userRepository.findAll(pageRequest);
+			model.addAttribute("users", page.getContent());
+			model.addAttribute("pageNo", pageNo);
+			model.addAttribute("totalPage", page.getTotalPages());
+			return "user/page"; //重導到 /WEB-INF/view/user/page.jsp
+		}
+
+			
 
 	// -----------------------------
 	// 以下是測試user的程式
